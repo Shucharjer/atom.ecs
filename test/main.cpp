14cfd541aff5b14c786b7588d7b207c09018f466
Shucharjer <Shucharjer@outlook.com>
2025-03-01 17:39:16 +0800

    update test case

--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <iostream>
 #include <string>
 #include <core.hpp>
@@ -6,30 +7,44 @@
 #include <ranges/to.hpp>
 #include "command.hpp"
 #include "queryer.hpp"
+#include "resources.hpp"
 #include "world.hpp"
 
 using namespace atom;
 
-const auto print = [](const auto& val) { std::cout << val << '\n'; };
+const auto print = [](const auto&... val) { ((std::cout << val << ' '), ...); };
 
 const auto newline = []() { std::cout << '\n'; };
 
 void startup(ecs::command& command, ecs::queryer& queryer) {
-    auto first_entity = command.spawn<std::string>(std::string{ "asoijdasjd" });
+    auto first_entity = command.spawn<std::string>("the first");
     print(first_entity);
-    auto second_entity = command.spawn<std::string>(std::string{ "the second entity has string" });
+    command.kill(first_entity);
+    auto second_entity = command.spawn<std::string>("the second entity has string");
     print(second_entity);
+    newline();
+    auto entities = queryer.query_all_of<>();
+    std::ranges::for_each(entities, print);
+    newline();
 }
 
 void update(ecs::command& command, ecs::queryer& queryer, float delta_time) {
+    command.spawn<std::string>();
     auto entities = queryer.query_any_of<std::string>();
     for (const auto& entity : entities) {
-        print(entity);
-        print(queryer.get<std::string>(entity));
+        print(
+            "I'm entity",
+            entity,
+            "my index is",
+            queryer.index(entity),
+            "my generation is",
+            queryer.generation(entity)
+        );
         newline();
     }
 
     command.detach<std::string>(entities.front());
+    command.set<resources::garbage_collect::enable_garbage_collect&&>({ true });
 }
 
 void shutdown(ecs::command& command, ecs::queryer& queryer) {
@@ -47,6 +62,7 @@ int main() {
     world.add_update(update);
     world.add_shutdown(shutdown);
     world.startup();
+    world.update(0.F);
     world.update(0.F);
     world.shutdown();
 
